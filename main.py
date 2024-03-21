7260c50f8bf47ed1dfa20ad0452ae21d9c6517d0
erudmila <144694341+erudmila@users.noreply.github.com>
2024-03-21 12:18:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+name = "Billy"
+print("We want to know if you like progamming!")
+print()
+print("Do you like programming " + name + "?")
+answer = input()
+print("Great, " + name + ", you said " + answer + "!")
+print("Let's learn some Python today")
+
+# How many output statements are used in the code?
+ # 4
+
+# What symbol is used for concatenation?
+ # Answer +
+
+# What is the purpose of the program?
+ # to ask if the viewer likes programming
+
+# What line(s) is assignment used on?
+ # lines 4 and 6
+
+# What symbol is used for assignment?
+ # Answer ? and !
+
+# What is the purpose of the empty print statement on line 3?
+ # Answer to keep a space between the lines that is shown in the console
